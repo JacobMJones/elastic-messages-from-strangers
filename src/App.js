f06365ddb7b7f80c997664f5a94665603ebe98f5
JacobMJones <mrjacobtaiwan@gmail.com>
2019-07-19 11:55:48 -0400

    Cleaned up formatting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import { StateProvider} from "./State/StateProvider";
 import Button from "./Button";
 import Text from "./Text"
-import InitialState from "./State/InitialState"
+import initialState from "./State/InitialState"
 import reducer from "./State/Reducer"
 function App() {
   return (
-    <StateProvider initialState={InitialState} reducer={reducer}>
+    <StateProvider initialState={initialState} reducer={reducer}>
       <Text/>
       <Button />
     </StateProvider>
--- src/Text.js
+++ src/Text.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useStateValue } from "./State/StateProvider";
 function Text() {
   const [{ numbers }] = useStateValue();
-  console.log("in text", numbers);
   return <p>I been clickered {numbers.count} times</p>;
 }
 export default Text;
